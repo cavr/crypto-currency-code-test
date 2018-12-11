33a72ea8dae768073373ba708a34951ceec667cf
Charles <amiguero809@gmail.com>
2018-12-11 23:49:55 +0100

    Improve CurrencyContainer

--- src/components/CurrencyContainer.js
+++ src/components/CurrencyContainer.js
@@ -1,66 +1,61 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { getCurrencies } from "../actions/CurrencyActions";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {getCurrencies} from "../actions/CurrencyActions";
 import PropTypes from "prop-types";
 import Currency from "./Currency";
 
 class CurrencyContainer extends Component {
-  componentDidMount() {
-    this.props.getCurrencies();
-    this.currencyInterval = setInterval(
-      () => this.props.getCurrencies(),
-      this.props.interval
-    );
-  }
-  componentWillUnmount() {
-    clearInterval(this.currencyInterval);
-  }
+    componentDidMount() {
+        this.props.getCurrencies();
+        this.currencyInterval = setInterval(
+            () => this.props.getCurrencies(),
+            this.props.interval
+        );
+    }
 
-  shouldComponentUpdate(newProps) {      
-    if (!newProps.currencies) {
-      return false;
+    componentWillUnmount() {
+        clearInterval(this.currencyInterval);
     }
-    if(!this.props.currencies){
-      return true;
-    }
-    for (const currency in newProps.currencies) {
-      if (
-        newProps.currencies[currency].color === this.props.defaultColor &&
-        newProps.currencies[currency].color !==
-          this.props.currencies[currency].color
-      ) {
-        return true;
-      }
-    }
-    return false;
-  }
 
-  render() {
-    return (
-      <div>
-        <Currency currencies={this.props.currencies} />
-      </div>
-    );
-  }
+    shouldComponentUpdate(newProps) {
+        if (!newProps.currencies) {
+            return false;
+        }
+        if (!this.props.currencies) {
+            return true;
+        }
+        for (const currency in newProps.currencies) {
+            if (newProps.currencies[currency].price_usd !== this.props.currencies[currency].price_usd) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    render(){
+        return (
+            <div>
+                <Currency currencies={this.props.currencies}/>
+            </div>
+        );
+    }
 }
 
 CurrencyContainer.defaultProps = {
-  interval: 30000,
-  defaultColor: 'white'
+    interval: 30000
 };
 
 CurrencyContainer.propTypes = {
-  interval: PropTypes.number,
-  defaultColor: PropTypes.string
+    interval: PropTypes.number,
 };
 
 const mapStateToProps = (state, props) => {
-  return {
-    currencies: state.currency
-  };
+    return {
+        currencies: state.currency
+    };
 };
 
 export default connect(
-  mapStateToProps,
-  { getCurrencies }
+    mapStateToProps,
+    {getCurrencies}
 )(CurrencyContainer);
